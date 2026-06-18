4cbcd786c1ca8513ae86c6e0d745f9b41159a1d0
rookie0422 <143528531+rookie0422@users.noreply.github.com>
2026-06-18 11:06:59 +0800

    build: omit source content from sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   target: 'node',
   entry: 'src/index.ts',
-  devtool: 'source-map',
+  devtool: 'nosources-source-map',
   context: __dirname,
   mode: 'development',
   output: {
